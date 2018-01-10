fff229a65ddf60a421afdac9997a9c9cd4ddc7ae
NewYoung <657851574@qq.com>
2018-01-10 21:33:45 +0800

    简单加载wasm文件并在js中执行

--- webassembly/loadWebAssembly.js
+++ webassembly/loadWebAssembly.js
@@ -1,15 +1,25 @@
-function loadWebAssembly(filename,imports = {}) {
-    fetch('demo0.wasm')//1.加载wasm文件
-        .then(response => response.arrayBuffer())//2.转成arraybuffer
-        .then(buffer => WebAssembly.compile(buffer))//3.编译
-        .then(module => {
-            imports.env = imports.env || {}
-            Object.assign(imports.env,{
-                memoryBase:0,
-                tableBase:0,
-                memory:new WebAssembly.Memory({inital:256,maximum:256}),
-                table:new WebAssembly.Table({inital:0,maximum:0,element:'anyfunc'})
+/**
+ * @param {String} path wasm 文件路径
+ * @param {Object} imports 传递到 wasm 代码中的变量
+ */
+function loadWebAssembly (path, imports = {}) {
+    return fetch(path)                                                                  //1.加载文件
+            .then(response => response.arrayBuffer())                                   //2.转成ArrayBuffer
+            .then(buffer => WebAssembly.compile(buffer))                                //3.编译
+            .then(module => {
+                imports.env = imports.env || {}
+                // 开辟内存空间
+                imports.env.memoryBase = imports.env.memoryBase || 0
+                if (!imports.env.memory) {
+                    imports.env.memory = new WebAssembly.Memory({ initial: 256 })
+                }
+                // 创建变量映射表
+                imports.env.tableBase = imports.env.tableBase || 0
+                if (!imports.env.table) {
+                    // 在 MVP 版本中 element 只能是 "anyfunc"
+                    imports.env.table = new WebAssembly.Table({ initial: 0, element: 'anyfunc' })
+                }
+
+                return new WebAssembly.Instance(module, imports)                       //4.创建WebAssembly实例
             })
-            return new WebAssembly.Instance(module,imports);//4.实例化
-        });
 }
