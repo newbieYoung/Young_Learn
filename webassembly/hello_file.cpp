15fc8d13e0a265ddbb7ad083455707e8338c30f6
NewYoung <657851574@qq.com>
2018-01-17 17:23:01 +0800

    Emscripten 例子学习

---
+++ webassembly/hello_file.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  FILE *file = fopen("./hello_sdl.cpp", "rb");
+  if (!file) {
+    printf("cannot open file\n");
+    return 1;
+  }
+  while (!feof(file)) {
+    char c = fgetc(file);
+    if (c != EOF) {
+      putchar(c);
+    }
+  }
+  fclose (file);
+  return 0;
+}
