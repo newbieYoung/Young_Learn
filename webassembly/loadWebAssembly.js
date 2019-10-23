71437aad6c7255f1d652947356cfab788ae6ca43
NewYoung <657851574@qq.com>
2019-10-23 20:24:32 +0800

    修改

--- webassembly/loadWebAssembly.js
+++ webassembly/loadWebAssembly.js
@@ -2,24 +2,28 @@
  * @param {String} path wasm 文件路径
  * @param {Object} imports 传递到 wasm 代码中的变量
  */
-function loadWebAssembly (path, imports = {}) {
-    return fetch(path)                                                                  //1.加载文件
-            .then(response => response.arrayBuffer())                                   //2.转成ArrayBuffer
-            .then(buffer => WebAssembly.compile(buffer))                                //3.编译
-            .then(module => {
-                imports.env = imports.env || {}
-                // 开辟内存空间
-                imports.env.memoryBase = imports.env.memoryBase || 0
-                if (!imports.env.memory) {
-                    imports.env.memory = new WebAssembly.Memory({ initial: 256 })
-                }
-                // 创建变量映射表
-                imports.env.tableBase = imports.env.tableBase || 0
-                if (!imports.env.table) {
-                    // 在 MVP 版本中 element 只能是 "anyfunc"
-                    imports.env.table = new WebAssembly.Table({ initial: 0, element: 'anyfunc' })
-                }
+function loadWebAssembly(path, imports = {}) {
+    return fetch(path) //1.加载文件
+        .then(response => response.arrayBuffer()) //2.转成ArrayBuffer
+        .then(buffer => WebAssembly.compile(buffer)) //3.编译
+        .then(module => {
+            imports.env = imports.env || {}
+            // 开辟内存空间
+            imports.env.memoryBase = imports.env.memoryBase || 0
+            if (!imports.env.memory) {
+                imports.env.memory = new WebAssembly.Memory({
+                    initial: 256
+                })
+            }
+            // 创建变量映射表
+            imports.env.tableBase = imports.env.tableBase || 0
+            if (!imports.env.table) {
+                imports.env.table = new WebAssembly.Table({ // 在 MVP 版本中 element 只能是 "anyfunc"
+                    initial: 0,
+                    element: 'anyfunc'
+                })
+            }
 
-                return new WebAssembly.Instance(module, imports)                       //4.创建WebAssembly实例
-            })
+            return new WebAssembly.Instance(module, imports) //4.创建WebAssembly实例
+        })
 }
