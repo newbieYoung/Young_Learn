84638804c2c1d62c943e2d653af227afe64853c5
NewYoung <657851574@qq.com>
2019-12-09 17:15:12 +0800

    webassembly c 语言卷积

--- webassembly/demo4.c
+++ webassembly/demo4.c
@@ -1,5 +1,6 @@
 /**
  * 卷积
+ * dst      计算结果
  * ker      卷积核
  * kerRows  卷积核行数
  * kerCols  卷积核列数
@@ -9,60 +10,69 @@
  * ----
  * 对于图片来说行数即高度、列数即宽度
  */
-float * convolution(float ker[], int kerRows, int kerCols, float mat[], int matRows, int matCols){
+void convolution(float dst[], float ker[], int kerRows, int kerCols, unsigned char mat[], int matRows, int matCols)
+{
   int len = 4;
   int matLen = matRows * matCols * len;
 
-  //暂不考虑非奇数尺寸卷积核
-  if (kerRows % 2 != 1 || kerCols % 2 != 1) {
-    return 0;
-  }
-
   //一般而言卷积核所有元素之和等于1；否则大于1时生成的图片亮度会增加，小于1时生成的图片亮度会降低
   float sums[4] = {0.0, 0.0, 0.0, 0.0};
-  
-  for (int z = 0; z < len; z++) {
-    for (int i = 0; i < kerRows; i++) {
-      for (int j = 0; j < kerCols; j++) {
-        int no = (i * kerCols  + j) * len;
+  for (int z = 0; z < len; z++)
+  {
+    for (int i = 0; i < kerRows; i++)
+    {
+      for (int j = 0; j < kerCols; j++)
+      {
+        int no = (i * kerCols + j) * len;
         sums[z] += ker[no + z];
       }
     }
   }
-
-  for (int z = 0; z < len; z++) {
-    for (int i = 0; i < kerRows; i++) {
-      for (int j = 0; j < kerCols; j++) {
-        int no = (i * kerCols  + j) * len;
+  for (int z = 0; z < len; z++)
+  {
+    for (int i = 0; i < kerRows; i++)
+    {
+      for (int j = 0; j < kerCols; j++)
+      {
+        int no = (i * kerCols + j) * len;
         ker[no + z] = ker[no + z] / sums[z];
       }
     }
   }
 
-  //卷积核中心
-  int cRow = (kerRows - 1) / 2;
-  int cCol = (kerCols - 1) / 2;
+  //卷积核中心、暂不考虑非奇数尺寸卷积核
+  int cRow = (int)((kerRows - 1) / 2);
+  int cCol = (int)((kerCols - 1) / 2);
 
   //卷积计算
-  for (int r = 0; r < matRows; r++) {
+  for (int r = 0; r < matRows; r++)
+  {
     int rs = r - cRow;
-    for (int c = 0; c < matCols; c++) {
+    for (int c = 0; c < matCols; c++)
+    {
       int cs = c - cCol;
       int noCur = (r * matCols + c) * len;
-      for (int z = 0; z < len; z++) {
+      for (int z = 0; z < len; z++)
+      {
         float sum = 0.0;
-        if (z == len - 1) {
+        if (z == len - 1)
+        {
           //透明通道不参与计算
-          sum = mat[noCur + z];
-        } else {
-          for (int i = 0; i < kerRows; i++) {
+          sum = mat[noCur + z] * 1.0;
+        }
+        else
+        {
+          for (int i = 0; i < kerRows; i++)
+          {
             int r1 = rs + i;
-            for (int j = 0; j < kerCols; j++) {
+            for (int j = 0; j < kerCols; j++)
+            {
               int c1 = cs + j;
 
               int n1 = (r1 * matCols + c1) * len;
-              float v1 = 0.0; //超出范围使用常量0代替
-              if (n1 + z >= 0 && n1 + z <= matLen - 1) {
+              unsigned char v1 = 0; //超出范围使用常量0代替
+              if (n1 + z >= 0 && n1 + z <= matLen - 1)
+              {
                 v1 = mat[n1 + z];
               }
 
@@ -76,10 +86,8 @@ float * convolution(float ker[], int kerRows, int kerCols, float mat[], int matR
             }
           }
         }
-        mat[noCur + z] = sum;
+        dst[noCur + z] = sum;
       }
     }
   }
-
-  return mat;
 }
