3eef2f67c665eceb020adc227d88c239d858dd9a
newbieYoung <657851574@qq.com>
2022-03-11 12:09:31 +0800

    feat: emcc demos multiple files

---
+++ emcc/demos/calculate_float.c
@@ -0,0 +1,9 @@
+float add_float(float a, float b)
+{
+    return a + b;
+}
+
+float minus_float(float a, float b)
+{
+    return a - b;
+}
---
+++ emcc/demos/calculate_int.c
@@ -0,0 +1,9 @@
+int add_int(int a, int b)
+{
+    return a + b;
+}
+
+int minus_int(int a, int b)
+{
+    return a - b;
+}
