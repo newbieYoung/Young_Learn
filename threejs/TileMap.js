e667bb3c4db0342cfffa0dda9a77db8b7e74945b
newbieYoung <657851574@qq.com>
2022-05-29 16:27:34 +0800

    feat: pixijs spine

--- threejs/TileMap.js
+++ threejs/TileMap.js
@@ -1,5 +1,4 @@
 import * as THREE from "../node_modules/three/build/three.module.js";
-import * as BufferGeometryUtils from "../node_modules/three/examples/jsm/utils/BufferGeometryUtils.js";
 import { KDTree } from "../algorithm/kd-tree/KDTree.js";
 
 // 瓦片地图
